425da612bf844a581656dca38b8a75d3cb666a24
Tu Nombre <tucorreo@example.com>
2024-05-11 10:05:13 -0500

    Ingrese la base del quiz

---
+++ Correcion.php
@@ -0,0 +1,10 @@
+<?php
+$respuestas_correctas = array(
+    'p0' => '2', 
+    'p1' => '3',
+    'p2' => '3', 
+    'p3' => '2', 
+    'p4' => '3', 
+    
+);
+$respuestas_contador=0;
---
+++ index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/png" href="imagenes/for.png">
+    <title>QUIZ </title>
+  </head>
+  <body>
+  <div class="title">
+  <h1>QUIZ 2</h1>
+  <p>Este quiz fue diseñado para saber el conocimiento general de las personas sobre futbol a nivel de preguntas historicas, esta compuesto por preguntas sobre nombres, estadisticas, resultados, etc.</p>
+  </div>
+  <div class="container">
+  <div class="questions1-section">
+    <section id="p0">
+    <h2>Pregunta 1:</h2>
+    <p>¿cual equipo de colombia tiene mas libertadores</p>
+    <label>
+    <input type="radio"  name="p0" value="1">Santa Fe
+    </label>
+    <label>
+    
+    <input type="radio"  name="p0" value="2">Atletico Nacional
+    </label>
+    <label>
+    <input type="radio"  name="p0" value="3">Once Caldas
+    </label>
+    </section>
+
+    <section id="p1">
+    <h2>Pregunta 2:</h2>
+    <p>¿cual equipo tiene mas champions league</p>
+    <label>
+    <input type="radio"  name="p1" value="1">Bayer munchen
+    </label>
+    <label>
+    <input type="radio"  name="p1" value="2">Milan
+    </label>
+    <label>
+    <input type="radio"  name="p1" value="3">Real Madrid
+    </label>
+    </section>
+    <section id="p2">
+    <h2>Pregunta 3:</h2>
+    <p>¿En cual de los siguientes clubes cristiano ronaldo hizo mayor cantgidad de goles?</p>
+    <label>
+    <input type="radio"  name="p2" value="1">Bayern Munchen
+    </label>
+    <label>
+    <input type="radio"  name="p2" value="2">Juventus
+    </label>
+    <label>
+    <input type="radio"  name="p2" value="3">Real Madrid
+    </label>
+    </section>
+    <section id="p3">
+    <h2>Pregunta 4:</h2>
+    <p>¿Cual equipo de inglaterra tiene mas trofeos de champions league?</p>
+    <label>
+    <input type="radio"  name="p3" value="1">Manchester United
+    </label>
+    <label>
+    <input type="radio"  name="p3" value="2">Liverpool
+    </label>
+    <label>
+    <input type="radio"  name="p3" value="3">chelsea
+    </label>
+    </section>
+    <section id="p4">
+    <h2>Pregunta 5:</h2>
+    <p>¿Que equipos jugaron la final de la chapions league 2008</p>
+    <label>
+    <input type="radio"  name="p4" value="1">Barcelona vs Real Madrid
+    </label>
+    <label>
+    <input type="radio"  name="p4" value="2">Milan vs Liverpool
+    </label>
+    <label>
+    <input type="radio"  name="p4" value="3">Manchester United vs Chelsea
+    </label>
+    </section>
+    </div>
+    
+    <button type="submit">Finalizar</button>
+    
+    <h3>Respuestas correctas: <span id="resultado"></span></h3>
+    
+
+
+
+  </div>
+  </body>
+</html>
